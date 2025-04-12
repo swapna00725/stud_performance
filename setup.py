8c99b69a5cf51f0f5817d81880862b2a22b8f266
Swapna Jyothi <mswapnaj@gmail.com>
2025-04-12 23:48:30 +0530

    setup and requirement

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements()->List[str]:
+    requirement_lst : List[str]=[]
+
+    try :
+        with open('requirements.txt','r') as file:
+            lines=file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                if requirement and requirement!='-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print('requirements.txt file not found')
+    return requirement_lst
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='swapna',
+    author_email='mswapnaj@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
+
+
