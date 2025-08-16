15d232ed913c6862b9f6b14ae19bc6692a6ec205
Swapna Jyothi <mswapnaj@gmail.com>
2025-08-16 23:20:40 +0530

    updated files

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ def get_requirements()->List[str]:
         with open('requirements.txt','r') as file:
             lines=file.readlines()
             for line in lines:
+                
                 requirement=line.strip()
                 if requirement and requirement!='-e .':
                     requirement_lst.append(requirement)
